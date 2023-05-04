b6749143a8e315b68392b640f3dd9aef8a08ce6c
ricardo <ricardodev10@yahoo.com>
2023-05-04 11:48:15 -0300

    :sparkles: init with authroutes

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
 import GlobalStyles from "./styles/global";
-import { MoviePreview } from "./pages/MoviePreview";
+import { Routes } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <MoviePreview />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 );
